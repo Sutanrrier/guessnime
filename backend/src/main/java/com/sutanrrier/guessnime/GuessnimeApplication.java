92a61425eba2a9a2d9d332e4352669229aad47c5
Sutanrrier <pabloryanbr0@gmail.com>
2022-07-22 17:02:40 -0300

    Project Created

---
+++ backend/src/main/java/com/sutanrrier/guessnime/GuessnimeApplication.java
@@ -0,0 +1,13 @@
+package com.sutanrrier.guessnime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessnimeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sutanrrier/guessnime/GuessnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sutanrrier.guessnime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
