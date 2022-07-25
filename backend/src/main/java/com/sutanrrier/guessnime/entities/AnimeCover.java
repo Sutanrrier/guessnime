c99a9964150efa7043ebe8d8308116042521644c
Sutanrrier <pabloryanbr0@gmail.com>
2022-07-25 14:01:08 -0300

    Recriada Entidade AnimeCover

--- backend/src/main/java/com/sutanrrier/guessnime/entities/AnimeCover.java
+++ backend/src/main/java/com/sutanrrier/guessnime/entities/AnimeCover.java
@@ -1,6 +1,7 @@
 package com.sutanrrier.guessnime.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,13 +17,14 @@ public class AnimeCover implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	private String title;
 	private String urlCover;
-	private String urlCoverNivel0;
-	private String urlCoverNivel1;
-	private String urlCoverNivel2;
-	private String urlCoverNivel3;
-	private String urlCoverNivel4;
+	private String url0;
+	private String url1;
+	private String url2;
+	private String url3;
+	private String url4;
 	
 	//Construtores
 	public AnimeCover() {
@@ -33,8 +35,16 @@ public class AnimeCover implements Serializable {
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 	public String getUrlCover() {
@@ -44,39 +54,56 @@ public class AnimeCover implements Serializable {
 		this.urlCover = urlCover;
 	}
 
-	public String getUrlCoverNivel0() {
-		return urlCoverNivel0;
+	public String getUrl0() {
+		return url0;
 	}
-	public void setUrlCoverNivel0(String urlCoverNivel0) {
-		this.urlCoverNivel0 = urlCoverNivel0;
-	}
-
-	public String getUrlCoverNivel1() {
-		return urlCoverNivel1;
-	}
-	public void setUrlCoverNivel1(String urlCoverNivel1) {
-		this.urlCoverNivel1 = urlCoverNivel1;
+	public void setUrl0(String url0) {
+		this.url0 = url0;
 	}
 
-	public String getUrlCoverNivel2() {
-		return urlCoverNivel2;
+	public String getUrl1() {
+		return url1;
 	}
-	public void setUrlCoverNivel2(String urlCoverNivel2) {
-		this.urlCoverNivel2 = urlCoverNivel2;
-	}
-
-	public String getUrlCoverNivel3() {
-		return urlCoverNivel3;
-	}
-	public void setUrlCoverNivel3(String urlCoverNivel3) {
-		this.urlCoverNivel3 = urlCoverNivel3;
+	public void setUrl1(String url1) {
+		this.url1 = url1;
 	}
 
-	public String getUrlCoverNivel4() {
-		return urlCoverNivel4;
+	public String getUrl2() {
+		return url2;
 	}
-	public void setUrlCoverNivel4(String urlCoverNivel4) {
-		this.urlCoverNivel4 = urlCoverNivel4;
+	public void setUrl2(String url2) {
+		this.url2 = url2;
+	}
+
+	public String getUrl3() {
+		return url3;
+	}
+	public void setUrl3(String url3) {
+		this.url3 = url3;
+	}
+
+	public String getUrl4() {
+		return url4;
+	}
+	public void setUrl4(String url4) {
+		this.url4 = url4;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AnimeCover other = (AnimeCover) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
