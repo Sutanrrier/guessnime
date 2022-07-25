8eda494dfbe793402e593dcaf729942a0ed642f1
Sutanrrier <pabloryanbr0@gmail.com>
2022-07-25 16:53:48 -0300

    GuessInput agora coleta AnimeCovers do banco

--- frontend/src/components/GuessInput/GuessInput.jsx
+++ frontend/src/components/GuessInput/GuessInput.jsx
@@ -1,23 +1,64 @@
 import "./GuessInput.css";
 
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 function GuessInput() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, setValue } = useForm();
+
+  const [guess, setGuess] = useState("");
+  const [covers, setCovers] = useState([]);
 
   const onSubmit = (data) => {
     console.log(data);
   };
 
+  function handleChangeInput(event) {
+    const actualGuess = event.target.value;
+    setGuess(actualGuess);
+
+    if (actualGuess.length > 3) {
+      const url = `http://localhost:8080/anime-covers/guess?title=${guess}`;
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          setCovers(data);
+        });
+    } else {
+      setCovers([]);
+    }
+  }
+
   return (
     <div>
+      <ul className="guessnime-anime-list">
+        {covers.map((data) => {
+          return (
+            <li
+              key={data.id}
+              id={data.id}
+              className="guessnime-anime-list-item"
+              onClick={() => {
+                setGuess(data.title);
+                setCovers([]);
+                setValue("guess", data.title);
+              }}
+            >
+              {data.title}
+            </li>
+          );
+        })}
+      </ul>
       <form className="guessnime-guess-form" onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
           name="guess"
           placeholder="🔎 Search an anime..."
           className="guessnime-guess-input"
-          {...register("guess", { required: true })}
+          value={guess}
+          {...register("guess")}
+          onChange={handleChangeInput}
         />
         <button
           type="submit"
