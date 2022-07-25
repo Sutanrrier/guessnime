0954221919560027d1d10daff5c26e1b3d930483
Sutanrrier <pabloryanbr0@gmail.com>
2022-07-24 21:49:10 -0300

    CORS Policy

--- backend/src/main/java/com/sutanrrier/guessnime/entities/AnimeCover.java
+++ backend/src/main/java/com/sutanrrier/guessnime/entities/AnimeCover.java
@@ -2,7 +2,6 @@ package com.sutanrrier.guessnime.entities;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
